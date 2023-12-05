8938f6dfeed24f488fec224d05cff00be48b13f3
Oskar <danila.tsymbal@mail.ru>
2023-12-05 14:51:57 +0300

    feat: added ability to easily scale amount of required rows

--- basics1/homework/main.go
+++ basics1/homework/main.go
@@ -4,28 +4,29 @@ import "github.com/ew0s/ewos-to-go-hw/basics1/homework/cell"
 
 // https://www.shellhacks.com/bash-colors/
 func main() {
-	c := cell.CreateCell("Название",
-		"Описание",
-		"1000 $",
-		"Москва",
-		"Нет",
+	c := cell.New(
+		cell.Row{"💬", "Название", "Станок"},
+		cell.Row{"📖", "Описание", "Станок для дерева"},
+		cell.Row{"💵", "Цена", "1000 $"},
+		cell.Row{"📍", "Локация", "Москва"},
+		cell.Row{"📦", "Доставка", "Нет"},
 		cell.Row{"🚀", "Опциональная строка 1", "Значение 1"},
 		cell.Row{"🎒", "Опциональная строка 2", "Значение 2"},
 	)
 
-	c.Draw(cell.Borderless,
+	c.Draw(c.Borderless(),
 		cell.ColorFunc(cell.LightGray.Background()),
 		cell.ColorFunc(cell.Purple.Foreground()),
 		cell.CharFunc(cell.Bold),
 	)
 
-	c.Draw(cell.Border,
+	c.Draw(c.Border(),
 		cell.ColorFunc(cell.LightGray.Background()),
 		cell.ColorFunc(cell.Purple.Foreground()),
 		cell.CharFunc(cell.Bold),
 	)
 
-	c.Draw(cell.StarredBorder,
+	c.Draw(c.StarredBorder(),
 		cell.ColorFunc(cell.LightGray.Background()),
 		cell.ColorFunc(cell.Purple.Foreground()),
 		cell.CharFunc(cell.Bold),
